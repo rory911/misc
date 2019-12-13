997c2bfe1dc14afa7382187bf91dddb9aa35264d
Rory Foster <rory911@austin.rr.com>
2019-12-13 06:04:39 -1000

    Additional code

--- bitcount.c
+++ bitcount.c
@@ -2,13 +2,11 @@
 #include <stdlib.h>
 
 #define	BYTE	8
-#define	ZERO	48
-#define	ONE	49
 #define	FIRST	1<<(BYTE - 1)
 #define LAST	1<<BYTE
 
 /* Number of bits set for a given positive integer N */
-unsigned int
+static unsigned int
 bitcount(unsigned int N)
 {
 	unsigned int total = 0;
@@ -21,16 +19,17 @@ bitcount(unsigned int N)
 	return (total);
 }
 
-/* Simple minded integer to binary unicode */
-char *
+/* Integer to binary unicode to beautify output.  Caller must free heap memory */
+static char *
 i2bin(unsigned int N)
 {
 	char *bin = (char *)calloc(BYTE, sizeof(char));
 
 	for (int i = BYTE - 1; i >= 0; --i) {
-		bin[i] = (N & 1) ? ONE : ZERO;
+		bin[i] = (N & 1) ? '1' : '0';
 		N >>= 1;
 	}
+
 	return (bin);
 }
 
@@ -40,8 +39,7 @@ main()
 	char *bits;
 
 	for (unsigned int i = FIRST; i < LAST; ++i) {
-		bits = i2bin(i);
-		printf("%3d(%s), bits=%u\n", i, bits, bitcount(i));
+		printf("%3d(%s), bitcount=%d\n", i, (bits = i2bin(i)), bitcount(i));
 		free(bits);
 	}
 }
---
+++ palindrome.go
@@ -0,0 +1,54 @@
+package main
+
+import ( "fmt" )
+
+const N = 32	// Upper limit of bit count
+
+//
+// Copy to slice and test the ends toward the middle with O(N) worst case,
+// and working storage has a 1:32 ratio to input size.
+//
+func is_pal1(val uint) bool {
+	var	siz int
+	var	slc [N]uint
+
+	for siz = 0; siz < N && val > 0; siz,val = siz+1,val>>1 {
+		slc[siz] = val & 1
+	}
+
+	// Leading zeros are stripped at 'siz'
+	fmt.Printf("%13s ", fmt.Sprint(slc[:siz]))
+
+	for i,j := 0,siz-1; i < j; i,j = i+1,j-1 {
+		if slc[i] != slc[j] {
+			return false
+		}
+	}
+	return true
+}
+
+//
+// Reverse bits and test for equality with O(N) worst case and 1:1 to input.
+//
+func is_pal2(val uint) bool {
+	fwd, rev := val, uint(0)
+	for ; val > 0; val >>= 1 {
+		rev <<= 1
+		if (val & 1) == 1 {
+			rev ^= 1
+		}
+	}
+	return fwd == rev
+}
+
+//
+// Bitwise palindrome as per Amazon interview of 12/10/19.  Alas, the
+// more space efficient approach occurred to me later. 
+//
+func main() {
+	var	i uint
+
+	for i = 0; i < N ; i++ {
+		fmt.Printf("%2d  %6v %6v\n", i, is_pal1(i), is_pal2(i))
+	}
+}
